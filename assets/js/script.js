661d7384f7c0f6524e531469812e888fa69644fc
Tyric Jones <tyrictjones@gmail.com>
2022-08-28 21:11:16 -0700

    added basic add/edit functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
 $('#currentDay').text(moment().format('dddd, MMM Do'));
+
+// time slot content area is clicked, add new content or edit existing content
+$('.row').on('click', function() {
+    var text = $(this)
+    .text()
+    .trim();
+
+    // replace div element with a new textarea
+    var textInput = $("<textarea>").addClass("form-control").val(text);
+    $(this).replaceWith(textInput);
+
+    // auto focus new element
+    textInput.trigger("focus");
+});
+
+// editable textarea is unfocused
+$(".row").on("blur", function() {
+    // get current value of textarea
+    var text = $(this).val();
+
+    // recreate div element
+    var taskDiv = $("<div>")
+    .addClass("row col-10")
+    .text(text);
+
+    // replace textarea with new content
+    $(this).replaceWith(taskDiv);
+});
